9a37ec69d714b9d2f0733fe8c530827731ea0d56
gervais <gervaisamoah@yahoo.com>
2024-12-16 16:07:34 +0000

    Experimenting Server Components

--- app/cabins/page.tsx
+++ app/cabins/page.tsx
@@ -1,5 +1,27 @@
+import Counter from '@/components/Counter';
 import React from 'react';
 
-export default function CabinsPage() {
-  return <div>CabinsPage</div>;
+type UserData = {
+  id: number;
+  name: string;
+  email: string;
+  website: string;
+};
+
+export default async function CabinsPage() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = (await res.json()) as UserData[];
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+      <Counter />
+    </div>
+  );
 }
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,15 +4,16 @@ import logo from '@/public/logo.png';
 
 function Logo() {
   return (
-    <Link href='/' className='flex items-center gap-4 z-10'>
+    <Link href="/" className="flex items-center gap-4 z-10">
       <Image
         src={logo}
-        height='60'
+        height="60"
         quality={100}
-        width='60'
-        alt='The Wild Oasis logo'
+        width="60"
+        alt="The Wild Oasis logo"
+        priority
       />
-      <span className='text-xl font-semibold text-primary-100'>
+      <span className="text-xl font-semibold text-primary-100">
         The Wild Oasis
       </span>
     </Link>
