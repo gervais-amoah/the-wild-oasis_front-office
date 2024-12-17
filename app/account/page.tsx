16d33f65cebe7e1374e59acc83a05d1da1fccfcf
gervais <gervaisamoah@yahoo.com>
2024-12-17 19:55:01 +0000

    Add new pages and some utility files

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -4,5 +4,11 @@ export const metadata = {
 };
 
 export default function AccountPage() {
-  return <div>Your Account</div>;
+  return (
+    <div>
+      <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+        Welcome back, firstName!
+      </h2>
+    </div>
+  );
 }
