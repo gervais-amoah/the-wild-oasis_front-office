e3d71326e63515e20303e7833cfa4b17e1b58bd6
gervais <gervaisamoah@yahoo.com>
2025-01-09 17:31:16 +0000

    Highlight current side navigation link

--- app/cabins/[cabinID]/page.tsx
+++ app/cabins/[cabinID]/page.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import { getCabin, getCabins } from '@/lib/data-service';
+import TextExpander from '@/components/TextExpander';
 
 export async function generateMetadata({ params }: CabinDetailsPageProps) {
   const { cabinID } = params;
@@ -46,7 +47,9 @@ export default async function CabinDetailsPage({
           {name}
         </h3>
 
-        <p className="text-lg text-primary-300 mb-10">{description}</p>
+        <p className="text-lg text-primary-300 mb-10">
+          <TextExpander>{description}</TextExpander>
+        </p>
 
         <ul className="flex flex-col gap-4 mb-7">
           <li className="flex gap-3 items-center">
